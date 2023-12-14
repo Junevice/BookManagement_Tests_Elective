af439b8c244b4a435998e0540f62a7aa96cd5e81
Junevice <june.webfreelance@gmail.com>
2023-12-14 09:58:59 +0100

    Add third test

--- src/main/kotlin/com/jicay/bookmanagement/domain/port/BookPort.kt
+++ src/main/kotlin/com/jicay/bookmanagement/domain/port/BookPort.kt
@@ -4,7 +4,7 @@ import com.jicay.bookmanagement.domain.model.Book
 
 interface BookPort {
     fun getAllBooks(): List<Book>
-    fun getBookByTitle(title: String): Book
+    fun getBookByTitle(title: String): Book?
     fun createBook(book: Book)
     fun reserveBook(title: String)
 }
